3807a65e877674c352b1d646d1cea1c6861b1825
Tim Twiest <hello@timtwiest.nl>
2023-01-29 23:11:59 +0100

    add some extra default styling

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 interface IProps {
   children: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export default function Card({ children }: IProps) {
   return (
-    <div className="rounded-xl border border-gray-200 bg-white shadow-md">
+    <div className="rounded-xl border border-gray-200 bg-white p-6 shadow-md">
       {children}
     </div>
   );
 }
 
 function CardHeader({ children }: IProps) {
-  return <div className="flex items-center justify-between">{children}</div>;
+  return (
+    <div className="justify-betwee flex items-center font-bold">{children}</div>
+  );
 }
 
 function CardBody({ children }: IProps) {
-  return <div className="p-4">{children}</div>;
+  return <div className="font-normal text-gray-400">{children}</div>;
 }
 
 Card.Header = CardHeader;
